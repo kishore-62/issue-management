6de06b58a9b099cab68ac8a5211e028ddc5351ef
kishore <kishorekumardasari6@gmail.com>
2026-01-26 19:22:56 +0530

    Test User API with Postman

---
+++ issue-management/src/main/java/com/kishore/issue_management/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.kishore.issue_management.controller;
+
+import com.kishore.issue_management.entity.User;
+import com.kishore.issue_management.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User createdUser = userService.createUser(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<User> getUserByEmail(@PathVariable String email) {
+        User user = userService.getUserByEmail(email);
+        return ResponseEntity.ok(user);
+    }
+}
