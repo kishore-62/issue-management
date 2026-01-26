45d68aa6e39e8b26c4d67bdf52603f4f51208318
kishore <kishorekumardasari6@gmail.com>
2026-01-26 17:56:02 +0530

    Add UserRepository

---
+++ issue-management/src/main/java/com/kishore/issue_management/entity/User.java
@@ -0,0 +1,29 @@
+package com.kishore.issue_management.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
